dba25b3d3160ec0d8d12054f1199b39df1c44acd
Davronbek2811 <davronjon176@gmail.com>
2025-08-13 15:32:04 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+alert('Sizda hozir random sonlar chiqishini boshladi😉')
+
+const Matem = Math.floor(Math.random() * 100) + 1
+
+alert(`Random orqali chiqan son: ${Matem}`)
+
+alert(`Biz sizga random sonini 2 kopaytirib(*) va 2 Bolib(/) bermoqichimiz!!!`)
+alert(`Random orqali chiqan son: ${Matem}
+biz uni kopaytirgan son: ${Matem * 2},
+biz uni bolganimizda son: ${Matem / 2}
+`)
